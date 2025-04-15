6aff8f6d4934fc9659ac7c1442a8c5093cdce8e8
augustinde <augustin.1201@outlook.fr>
2025-04-15 19:19:07 +0200

    feat: adding translation for pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,7 +14,7 @@ import {SliderService} from '../../services/slider.service';
 import {Slider} from '../../models/slider';
 import {ArticleTiny} from '../../models/article/articleTiny';
 import {MatchGroupByDate} from '../../models/match/matchGroupByDate';
-import {TranslatePipe} from '@ngx-translate/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
